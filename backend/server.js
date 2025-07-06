fb3b0b49e20353a58dedb8229ec518f60cf9a83e
mclanecorp <darkmienworld@gmail.com>
2025-07-06 17:53:57 +0200

    probleme cors deploiement

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ const Database = require("better-sqlite3");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://music-tka8.onrender.com'
+}));
 app.use(express.json()); // To parse JSON request bodies
 
 // Initialize SQLite Database
